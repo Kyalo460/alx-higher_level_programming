999dab5e0374a7abbb45b9ef0d48efed9c552734
kyalo460 <kyalo460@gmail.com>
2023-09-05 19:11:22 +0000

    prints combinations of two digit integers

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+for num in range(0, 9):
+    for num2 in range(num+1, 10):
+        print("{}{}".format(num, num2), end='')
+
+        if num != 8:
+            print(", ", end='')
+        else:
+            print('')
