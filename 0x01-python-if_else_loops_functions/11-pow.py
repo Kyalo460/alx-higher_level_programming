14748241c1ffadaf78a892d77fbe56bf81c7a199
kyalo460 <kyalo460@gmail.com>
2023-09-06 15:01:55 +0000

    Returns a raised to b

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -3,16 +3,4 @@
 
 def pow(a, b):
     """Returns a raised to b."""
-    if b == 0:
-        return 1
-
-    if b < 0:
-        a = 1 / a
-        b = -b
-
-    n = a ** b
-
-    if a == 0.1:
-        n = round(n, 2)
-
-    return n
+    return a ** b
