ab4014015efccf424cb457a45d1e0a459dfe9572
kyalo460 <kyalo460@gmail.com>
2023-09-07 21:41:00 +0000

    Replace element and return copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+    """Replaces an element and returns a copy"""
+
+    limit = len(my_list) - 1
+    new_list = my_list.copy()
+
+    if idx > limit or idx  < 0:
+        return new_list
+
+    new_list[idx] = element
+
+    return new_list
