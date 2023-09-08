a1c51c00e26c1bbf42a4ad2402718ab5313e23bb
kyalo460 <kyalo460@gmail.com>
2023-09-08 04:36:17 +0000

    prints elements in a matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    """Prints elements in a matrix"""
+    if len(matrix[0]) == 0:
+        print('')
+        return 1
+
+    for x in range(0, len(matrix)):
+        for y in matrix[x]:
+            print(f"{y}", end='')
+
+            if y == matrix[x][len(matrix[x]) - 1]:
+                continue
+            print(' ', end='')
+
+        print('')
