aae3a71c64b4e86d520c36dcc9732edc525fe836
kyalo460 <kyalo460@gmail.com>
2023-09-08 18:01:50 +0000

    Added condition to check if list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,9 +4,8 @@
 def print_reversed_list_integer(my_list=[]):
     """Prints a list in reverse order"""
 
-    my_copy = my_list.copy()
+    if isinstance(my_list, list):
+        my_list.reverse()
 
-    my_copy.reverse()
-
-    for rev in my_copy:
-        print("{:d}".format(rev))
+        for rev in my_list:
+            print("{:d}".format(rev))
