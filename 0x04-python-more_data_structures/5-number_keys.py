3f1010b8b961aa711c85ef044f3731b6c66c4867
kyalo460 <kyalo460@gmail.com>
2023-09-09 18:42:16 +0000

    Returns the number of keys in a dictionary

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def number_keys(a_dictionary):
+    """Returns the number of keys in a dictionary."""
+
+    new_list = list(a_dictionary)
+
+    num = len(new_list)
+
+    return num
