e4a23f69761088223ea42251de186f9182b984a8
kyalo460 <kyalo460@gmail.com>
2023-09-09 18:23:54 +0000

    Makes a set from two sets with elements exclusively present in each set

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bun/python3
+
+
+def only_diff_elements(set_1, set_2):
+    """Returns a set of all elements present in only one set."""
+
+    od_set = {x for x in set_1 if x not in set_2}
+    od_set.update({x for x in set_2 if x not in set_1})
+    return od_set
