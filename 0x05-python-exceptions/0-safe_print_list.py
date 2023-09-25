e678ad000fd7ca5db30fffd642e6053a75aa1b7c
kyalo460 <kyalo460@gmail.com>
2023-09-25 11:06:45 +0000

    Tweaked return value

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,7 +11,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[a]), end='')
 
         print("")
-        return a
+        return a + 1
 
     except IndexError:
         print("")
