9a145ca5022ce7705f1f728b84fd7ab98360ea6c
kyalo460 <kyalo460@gmail.com>
2023-09-25 11:31:05 +0000

    Checked for TypeError exception

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -6,8 +6,8 @@ def safe_print_integer(value):
 
     try:
         print("{:d}".format(value))
-        
+
         return True
 
-    except ValueError:
+    except (ValueError, TypeError):
         return False
