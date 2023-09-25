a09623c55e7e1a69677f48104b5e1e76ca8c591f
kyalo460 <kyalo460@gmail.com>
2023-09-25 11:09:41 +0000

    Tweaked return value

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,15 +4,16 @@
 def safe_print_list(my_list=[], x=0):
     """A function that prints x elements of a list."""
 
-    a = 0
+    num = 0
 
     try:
         for a in range(0, x):
             print("{}".format(my_list[a]), end='')
+            num += 1
 
         print("")
-        return a + 1
+        return num
 
     except IndexError:
         print("")
-        return a
+        return num
