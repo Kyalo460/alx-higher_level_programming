87c2b63dab19cdfd7e1709e23cd507bd341c5893
kyalo460 <kyalo460@gmail.com>
2023-09-26 13:52:17 +0000

    Private field

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a class Square."""
+
+
+class Square:
+    """Inside the class Square.
+
+    Attribute: __size.
+    """
+
+    def __init__(self, size):
+        """Make a private variable."""
+        self.__size = size
