5a6c901f127765f5d9377400aead7c444f6f4ce1
kyalo460 <kyalo460@gmail.com>
2023-09-26 17:18:05 +0000

    Adds spaces in each loop

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -50,6 +50,8 @@ class Square:
             print("")
             return
 
+        for n in range(0, self.__position[1]):
+            print("")
         for i in range(0, self.__size):
             for k in range(0, self.__position[0]):
                 print(" ", end='')
