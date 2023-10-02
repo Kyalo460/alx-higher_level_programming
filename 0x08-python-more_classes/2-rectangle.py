420dc64ee42ca0c940ad42268f6449f9739c7167
kyalo460 <kyalo460@gmail.com>
2023-10-02 11:14:27 +0000

    Added 2 methods area() and perimeter()

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -42,7 +42,7 @@ class Rectangle:
 
     def perimeter(self):
         """Returns the perimeter of a rectangle."""
-        if self.width == 0 and self.height == 0:
+        if self.width == 0 or self.height == 0:
             return 0
 
         return (self.width * 2) + (self.height * 2)
