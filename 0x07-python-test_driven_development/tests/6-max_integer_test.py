ee8d2a47b681b183e65be14013169491b1e00b2a
kyalo460 <kyalo460@gmail.com>
2023-10-03 18:37:01 +0000

    Added more tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Unittest for max_integer([...])"""
-import sys
+
 import unittest
-sys.path.append("/home/vagrant/alx-higher_level_programming/0x07-python-test_driven_development")
 max_integer = __import__('6-max_integer').max_integer
+
 
 class TestMaxInteger(unittest.TestCase):
     """This class will have functions that test if the integer
@@ -14,10 +14,19 @@ class TestMaxInteger(unittest.TestCase):
         """Test whether the value returned is correct."""
         self.assertEqual(max_integer([1, 2, 3, 4]), max([1, 2, 3, 4]))
         self.assertEqual(max_integer([0.1, 0.2, 1.1]), max([0.1, 0.2, 1.1]))
-        
+
     def test_empty(self):
         """Should return None if given empty list."""
         self.assertEqual(max_integer([]), None)
+
+    def test_max_at_begginning(self):
+        """Test a list with a beginning max value."""
+        max_at_beginning = [4, 3, 2, 1]
+        self.assertEqual(max_integer(max_at_beginning), 4)
+
+    def test_one_element(self):
+        """Tests a list with one value."""
+        self.assertEqual(max_integer([2]), 2)
 
 
 if __name__ == "__main__":
