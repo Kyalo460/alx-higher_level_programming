6fd5250f9c4b0cead70b6fd4867d6754aad2b0eb
Kyalo460 <kyalo460@gmail.com>
2023-10-09 22:37:17 +0300

    Added square bracketd in __str__ string

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -16,4 +16,4 @@ class Square(Rectangle):
 
     def __str__(self):
         """Returns a string to print."""
-        return "Square {}/{}".format(self.__size, self.__size)
+        return "[Square] {}/{}".format(self.__size, self.__size)
