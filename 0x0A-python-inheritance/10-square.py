9e2aa27d9ed0e5a841c556f5c9bddaff4a1be7b2
Kyalo460 <kyalo460@gmail.com>
2023-10-09 22:30:38 +0300

    Class Squre that indirectly inherits

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""The class Square will inherit from Rectangle."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Init method."""
+
+    def __init__(self, size):
+        self.__size = size
+        super().integer_validator("size", self.__size)
+
+    def area(self):
+        """Returns the area of a square."""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Returns a string to print."""
+        return "Square {}/{}".format(self.__size, self.__size)
