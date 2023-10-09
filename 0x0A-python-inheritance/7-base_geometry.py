26cd11b6821bf7e015b1547640e0722a56bcfb69
Kyalo460 <kyalo460@gmail.com>
2023-10-09 18:30:25 +0300

    Validated the value using a method

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Making a class called BaseGeometry."""
+
+
+class BaseGeometry:
+    """Defining methods and attributes."""
+
+    def area(self):
+        """Raises an exception + an error message."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates the value if it is an integer and is greater than 0."""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+
+        if value < 0:
+            raise ValueError("{} must be greater than 0".format(name))
