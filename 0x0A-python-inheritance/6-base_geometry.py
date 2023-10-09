031e9f38029142dfc7cc8b63eb5a46dcdc643883
Kyalo460 <kyalo460@gmail.com>
2023-10-09 18:13:14 +0300

    Raise an exception with message

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Making a class called BaseGeometry."""
+
+
+class BaseGeometry:
+    """Defining methods and attributes."""
+
+    def area(self):
+        """Raises an exception + an error message."""
+        raise Exception("area() is not implemented")
+    
