1922c09fc19a55c885941330bba4d8d1db14f03d
Kyalo460 <kyalo460@gmail.com>
2023-10-09 22:50:21 +0300

    Added test case for duplicates + shortened method

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -10,10 +10,4 @@ class Square(Rectangle):
         self.__size = size
         super().integer_validator("size", self.__size)
 
-    def area(self):
-        """Returns the area of a square."""
-        return self.__size * self.__size
-
-    def __str__(self):
-        """Returns a string to print."""
-        return "Square {}/{}".format(self.__size, self.__size)
+        Rectangle.__init__(self, self.__size, self.__size)
