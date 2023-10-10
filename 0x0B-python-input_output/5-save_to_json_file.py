835dab553e57694f50fb9700c93e484f42c3ac93
Kyalo460 <kyalo460@gmail.com>
2023-10-10 15:03:27 +0300

    open file before using json

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,7 +6,6 @@ import json
 def save_to_json_file(my_obj, filename):
     """writes or overwrites an 'my_obj' to 'filename'."""
 
-    j_string = json.dumps(my_obj)
-
     with open(filename, 'w', encoding='utf-8') as f:
+        j_string = json.dumps(my_obj)
         f.write(j_string)
