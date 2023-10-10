7263f5dd912b966687b26edd76eae41c2be14782
Kyalo460 <kyalo460@gmail.com>
2023-10-10 14:01:10 +0300

    Creates a new file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A function that writes to a file."""
+
+
+def write_file(filename="", text=""):
+    """Creates a newfile or overwrites the existing one."""
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(text)
