31d01b76080eba5d7cd4cec911ccba9fd1865679
Kyalo460 <kyalo460@gmail.com>
2023-10-11 15:58:35 +0300

    Formated the document

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-"""A class Student that contains a students
-details.
+"""A class Student that contains
+a students details.
 """
 
+
 class Student:
-    """Will contain attributes that define the student
-    and a method to retrieve the student's details.
+    """Will contain attributes that defines
+    the student and a method to retrieve
+    the student's details.
     """
 
     def __init__(self, first_name, last_name, age):
-        """assigns values to attributes when class is
-        instatiated."""
+        """assigns values to attributes when
+        class is instatiated.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Returns a dictionary that has attribute:value."""
+        """Returns a dictionary that has 
+        attribute:value.
+        """
         return vars(self)
