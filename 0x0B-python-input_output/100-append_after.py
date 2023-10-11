526bc491600f65db12c50692e7d74fa953c73fdc
Kyalo460 <kyalo460@gmail.com>
2023-10-11 22:13:48 +0300

    Adds a string if subset of is found in file

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+"""A method that will append a certain line
+after a line that contains a specific string.
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Opens 'filename', searches for 'search_string' then
+    adds 'new_string' after the line that has 'search_string.
+    """
+
+    with open(filename, 'r', encoding='utf-8') as f:
+        list_of_lines = f.readlines()
+
+    new_list = []
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        for line in list_of_lines:
+            new_list.append(line)
+            if search_string in line:
+                new_list.append(new_string)
+
+        new_file_lines = "".join(new_list)
+        f.write(new_file_lines)
