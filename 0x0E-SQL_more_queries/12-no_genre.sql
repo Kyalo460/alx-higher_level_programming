33afd1494dd9819b23c344975ab40d1f3d8b23f4
kyalo460 <kyalo460@gmail.com>
2023-11-15 19:33:36 +0300

    Pushing even more files

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- a script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+SELECT ts.title, tg.genre_id
+FROM tv_shows AS ts
+LEFT JOIN tv_show_genres AS tg
+ON ts.id = tg.show_id
+WHERE tg.genre_id IS NULL;
