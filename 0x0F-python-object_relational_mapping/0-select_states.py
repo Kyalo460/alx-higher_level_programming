ac371a00a7a6207300eb26aedb30b065bc70b42a
Kyalo460 <Kyalo460@gmail.com>
2023-12-15 19:41:24 +0300

    shortened code

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,9 +8,6 @@ if __name__ == "__main__":
                          user=sys.argv[1], passwd=sys.argv[2],
                          database=sys.argv[3])
     cur = db.cursor()
-
     cur.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cur.fetchall()
-
-    for row in rows:
+    for row in cur.fetchall():
         print(row)
