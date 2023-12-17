40c6df315ee22b86758cf34c3e40c5dc3a925e3e
Kyalo460 <Kyalo460@gmail.com>
2023-12-17 19:52:55 +0300

    adds a new row to the table

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""adds a new row to a table in a database."""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(engine)
+    session = Session()
+    
+    new_object = State(name='Louisiana')
+    session.add(new_object)
+    session.commit()
+    print(new_object.id)
