72dd4c7668159c9dfa26b0472e8db66c7f9b1d77
Kyalo460 <Kyalo460@gmail.com>
2023-12-17 21:06:35 +0300

    added new class (table) and joined the two tables

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""class definition of a City."""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from model_state import Base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """schema for the cities table."""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, unique=True,
+                nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
