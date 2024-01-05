12b06dfafde8dc8c41faea8349095ce669918213
Kyalo460 <Kyalo460@gmail.com>
2024-01-05 16:37:28 +0300

    Finds the peak in a list

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""A method that finds the peak in list of integers"""
+
+
+def find_peak(list_of_integers):
+    """list_of_integers should not be empty"""
+    size = len(list_of_integers)
+    if size == 0:
+        return
+    if size == 1:
+        return list_of_integers[0]
+    if size == 2:
+        return max(list_of_integers)
+
+
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak <= list_of_integers[mid -1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
+
