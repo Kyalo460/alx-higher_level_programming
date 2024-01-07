07755f5a729858339f0022cdefbb59e645051ccd
Kyalo460 <Kyalo460@gmail.com>
2024-01-07 14:02:41 +0300

    imported outside the if statement

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+import requests
+from sys import argv
 """Python script that takes in a URL, sends a request to the URL and displays"""
 
 
 if __name__ == "__main__":
-    import requests
-    from sys import argv
     r = requests.get(argv[1])
     print(r.headers.get('X-Request-Id'))
