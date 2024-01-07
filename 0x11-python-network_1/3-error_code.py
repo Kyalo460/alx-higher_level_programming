e3ac9539202660c39487411c09198ca85fa40194
Kyalo460 <Kyalo460@gmail.com>
2024-01-07 14:35:38 +0300

    Used Request first

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """Sends a request to the URL and displays the body of the response"""
-
+import sys
+import urllib.request
+import urllib.error
 
 if __name__ == "__main__":
-    import sys
-    import urllib.request
-    import urllib.error
-
     url = sys.argv[1]
 
+    request = urllib.request.Request(url)
     try:
-        with urllib.request.urlopen(url) as response:
+        with urllib.request.urlopen(request) as response:
             print(response.read().decode("utf-8"))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
