ba0afec183a99708835a5dbbc5032300ec9d835e
Kyalo460 <Kyalo460@gmail.com>
2024-01-07 14:04:42 +0300

    used urllib

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import requests
+import requests.urllib
 from sys import argv
 """Python script that takes in a URL, sends a request to the URL and displays"""
 
 
 if __name__ == "__main__":
-    r = requests.get(argv[1])
+    r = requests.urllib.urlopen(argv[1])
     print(r.headers.get('X-Request-Id'))
