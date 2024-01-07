d2047b5fa304810de4bc742b44bd54d0376d94dc
Kyalo460 <Kyalo460@gmail.com>
2024-01-07 15:09:19 +0300

    shortened code

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""sends a request to the URL and displays the value of the variable X-Request-Id."""
+"""sends a request and displays the value of X-Request-Id."""
 import requests
 
 
