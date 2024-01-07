f4a3d11b67080f28c27f93bdd3221975ba1c07df
Kyalo460 <Kyalo460@gmail.com>
2024-01-07 13:55:24 +0300

    tweaked code a bit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,8 +2,10 @@
 """Python script that fetches https://intranet.hbtn.io/status"""
 import urllib.request
 
+
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
         html = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
